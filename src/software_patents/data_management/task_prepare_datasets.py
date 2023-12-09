269e23280d002271cb204e1dd17befc85f89cd67
Tobias Raabe <raabe@posteo.de>
2023-12-10 00:59:42 +0100

    fix.

--- src/software_patents/data_management/task_prepare_datasets.py
+++ src/software_patents/data_management/task_prepare_datasets.py
@@ -11,77 +11,57 @@ from software_patents.config import Mode
 from software_patents.config import ProjectMode
 from upath import UPath
 
+_BaseURL = "http://dl.dropboxusercontent.com/s"
+
 if ProjectMode == Mode.REPLICATION:
     data_catalog.add(
         "indicators_abstract",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/ckleerbtm54ddpm/indicators_abstract.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/ckleerbtm54ddpm/indicators_abstract.pkl?dl=0")
         ),
     )
     data_catalog.add(
         "indicators_description_1",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/tfhzex5o18ocugu/indicators_description_1.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/tfhzex5o18ocugu/indicators_description_1.pkl?dl=0")
         ),
     )
     data_catalog.add(
         "indicators_description_2",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/ayvvzdzr8kihx4e/indicators_description_2.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/ayvvzdzr8kihx4e/indicators_description_2.pkl?dl=0")
         ),
     )
     data_catalog.add(
         "indicators_description_3",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/frnos85yq97sps4/indicators_description_3.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/frnos85yq97sps4/indicators_description_3.pkl?dl=0")
         ),
     )
     data_catalog.add(
         "indicators_description_4",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/hsswas2hgwvb8et/indicators_description_4.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/hsswas2hgwvb8et/indicators_description_4.pkl?dl=0")
         ),
     )
     data_catalog.add(
         "indicators_description_5",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/42but1qhjil1trg/indicators_description_5.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/42but1qhjil1trg/indicators_description_5.pkl?dl=0")
         ),
     )
     data_catalog.add(
         "indicators_title",
-        PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/0fxqnvyhsljwprt/indicators_title.pkl?dl=0"
-            )
-        ),
+        PickleNode(path=UPath(f"{_BaseURL}/0fxqnvyhsljwprt/indicators_title.pkl?dl=0")),
     )
     data_catalog.add(
         "patent",
-        PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/atu4974la2p8d5s/patent.pkl?dl=0"
-            )
-        ),
+        PickleNode(path=UPath(f"{_BaseURL}/atu4974la2p8d5s/patent.pkl?dl=0")),
     )
     data_catalog.add(
         "indicators_summary",
         PickleNode(
-            path=UPath(
-                "http://dl.dropboxusercontent.com/s/8mffjyvintl757a/indicators_summary.pkl?dl=0"
-            )
+            path=UPath(f"{_BaseURL}/8mffjyvintl757a/indicators_summary.pkl?dl=0")
         ),
     )
 
